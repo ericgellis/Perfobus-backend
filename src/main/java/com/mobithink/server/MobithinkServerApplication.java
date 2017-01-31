4f635bbced3a5cd7c0e6d4ccb3cdf568ba49d327
axel thiel <taxelt@gmail.com>
2017-01-31 11:04:52 +0100

    first commit

---
+++ src/main/java/com/mobithink/server/MobithinkServerApplication.java
@@ -0,0 +1,12 @@
+package com.mobithink.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobithinkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobithinkServerApplication.class, args);
+	}
+}
