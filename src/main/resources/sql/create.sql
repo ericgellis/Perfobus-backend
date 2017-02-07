817823946011e536322ee6bc0fe78eefc9b3c257
axel thiel <taxelt@gmail.com>
2017-02-07 09:52:39 +0100

    Up create.sql file

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -7,98 +7,26 @@ CREATE SEQUENCE public.mobithink_uid
     CACHE 1;
 
 -- drop tables of link --
-DROP TABLE IF EXISTS public."city_line_link";
-DROP TABLE IF EXISTS public."event_place_link";
 DROP TABLE IF EXISTS public."line_station_link";
-DROP TABLE IF EXISTS public."line_trip_link";
-DROP TABLE IF EXISTS public."place_station_data_link";
-DROP TABLE IF EXISTS public."station_data_station_link";
-DROP TABLE IF EXISTS public."station_data_trip_link";
-DROP TABLE IF EXISTS public."trip_event_link";
 
 -- drop entities tables --
 DROP TABLE IF EXISTS public."bus_line";
 DROP TABLE IF EXISTS public."city";
 DROP TABLE IF EXISTS public."event";
-DROP TABLE IF EXISTS public."place";
+DROP TABLE IF EXISTS public."picture";
+DROP TABLE IF EXISTS public."rolling_point";
 DROP TABLE IF EXISTS public."station";
 DROP TABLE IF EXISTS public."station_data";
 DROP TABLE IF EXISTS public."trip";
 
 -- create tables of link --
 
-CREATE TABLE public.city_line_link
-(
-    id bigint NOT NULL,
-    city_id bigint NOT NULL,
-    bus_line_id bigint NOT NULL,
-    CONSTRAINT city_line_link_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE public.event_place_link
-(
-    id bigint NOT NULL,
-    place_id bigint NOT NULL,
-    event_id bigint NOT NULL,
-    CONSTRAINT event_place_link_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE public.line_station_link
-(
-    id bigint NOT NULL,
-    bus_line_id bigint NOT NULL,
-    station_id bigint NOT NULL,
-    CONSTRAINT line_sation_link_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE public.line_trip_link
-(
-    id bigint NOT NULL,
-    bus_line_id bigint NOT NULL,
-    trip_id bigint NOT NULL,
-    CONSTRAINT line_trip_link_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE public.place_station_data_link
-(
-    id bigint NOT NULL,
-    place_id bigint NOT NULL,
-    station_data_id bigint NOT NULL,
-    CONSTRAINT place_station_data_link_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE public.station_data_station_link
-(
-    id bigint NOT NULL,
-    station_id bigint NOT NULL,
-    station_data_id bigint NOT NULL,
-    CONSTRAINT station_data_station_link_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE public.station_data_trip_link
-(
-    id bigint NOT NULL,
-    station_data_id bigint NOT NULL,
-    trip_id bigint NOT NULL,
-    CONSTRAINT station_data_trip_link_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE public.trip_event_link
-(
-    id bigint NOT NULL,
-    trip_id bigint NOT NULL,
-    event_id bigint NOT NULL,
-    CONSTRAINT trip_event_link_pkey PRIMARY KEY (id)
-);
-
-
--- create entities tables --
-
 CREATE TABLE public.bus_line
 (
     id bigint NOT NULL,
     name text COLLATE pg_catalog."default" NOT NULL,
     creation_date bigint NOT NULL,
+    city_id bigint NOT NULL,
     CONSTRAINT bus_line_pkey PRIMARY KEY (id)
 );
 
@@ -115,18 +43,39 @@ CREATE TABLE public.event
     name text COLLATE pg_catalog."default" NOT NULL,
     start_time bigint NOT NULL,
     end_time bigint,
-    start_place_id bigint NOT NULL,
-    end_place_id bigint,
-    station_id bigint,
+    station_data_id bigint,
+    trip_id bigint NOT NULL,
+    gps_lat bigint,
+    gps_long bigint,
     CONSTRAINT event_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE public.place
+CREATE TABLE public.line_station_link
 (
     id bigint NOT NULL,
-    gps_lat bigint NOT NULL,
-    gps_long bigint NOT NULL,
-    CONSTRAINT place_pkey PRIMARY KEY (id)
+    bus_line_id bigint NOT NULL,
+    station_id bigint NOT NULL,
+    CONSTRAINT line_sation_link_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.picture
+(
+    id bigint NOT NULL,
+    station_data_id bigint,
+    event_id bigint,
+    picture_id bigint NOT NULL,
+    CONSTRAINT picture_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.rolling_point
+(
+    id bigint NOT NULL,
+    point_time bigint,
+    traffic integer,
+    trip_id bigint NOT NULL,
+    gps_lat bigint,
+    gps_long bigint,
+    CONSTRAINT rolling_point_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE public.station
@@ -144,6 +93,10 @@ CREATE TABLE public.station_data
     start_time bigint NOT NULL,
     end_time bigint NOT NULL,
     step integer,
+    station_name text COLLATE pg_catalog."default",
+    trip_id bigint NOT NULL,
+    gps_lat bigint,
+    gps_long bigint,
     CONSTRAINT station_data_pkey PRIMARY KEY (id)
 );
 
@@ -156,7 +109,12 @@ CREATE TABLE public.trip
     atmo integer,
     temperature integer,
     weather text COLLATE pg_catalog."default",
-    direction text COLLATE pg_catalog."default",
     vehicle_capacity integer,
+    bus_line_name text COLLATE pg_catalog."default" NOT NULL,
+    city_name text COLLATE pg_catalog."default" NOT NULL,
+    start_gps_lat bigint,
+    start_gps_long bigint,
+    end_gps_lat bigint,
+    end_gps_long bigint,
     CONSTRAINT trip_pkey PRIMARY KEY (id)
 );
