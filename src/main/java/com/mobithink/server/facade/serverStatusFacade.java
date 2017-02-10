a35f2a01507017f4152a38ffa2be45c3041d730d
axel thiel <taxelt@gmail.com>
2017-02-10 09:28:58 +0100

    test to suppress clouddataconf and fixedpostgres

---
+++ src/main/java/com/mobithink/server/facade/serverStatusFacade.java
@@ -0,0 +1,31 @@
+package com.mobithink.server.facade;
+
+import com.mobithink.server.exeption.MobithinkBusinessException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by athiel on 10/02/2017.
+ */
+
+@RestController
+@RequestMapping(value = "/mobithink/server" )
+public class serverStatusFacade {
+
+    /**
+     *
+     * GET. wake up server
+     *
+     *
+     * @return  "success" or null
+     *
+     *
+     */
+    @GetMapping(path = "/wakeup", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity<String> findAllCity() throws MobithinkBusinessException {
+       return ResponseEntity.ok("success");
+    }
+}
