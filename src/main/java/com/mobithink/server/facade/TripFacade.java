8e3d18ee8a5cf787549e48c6cf1ab7a4304b43e6
axel Thiel <taxelt@gmail.com>
2017-02-14 18:27:02 +0100

    Correct bug

--- src/main/java/com/mobithink/server/facade/TripFacade.java
+++ src/main/java/com/mobithink/server/facade/TripFacade.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -24,178 +25,203 @@ import java.util.List;
 @RequestMapping(value = "/mobithink/trip" )
 public class TripFacade {
 
-    @Resource
-    TripService tripService;
+	@Resource
+	TripService tripService;
 
-    @Resource
-    StationDataService stationDataService;
+	@Resource
+	StationDataService stationDataService;
 
-    @Resource
-    EventService eventService;
+	@Resource
+	EventService eventService;
 
-    @Resource
-    RollingPointService rollingPointService;
+	@Resource
+	RollingPointService rollingPointService;
 
-    @Resource
-    PictureService pictureService;
+	@Resource
+	PictureService pictureService;
 
-    @Resource
-    BusLineService busLineService;
-
-
-    /**
-     *
-     * POST. Register a new TripDTO.
-     *
-     * @param tripDTO object
-     *
-     * @return status 201 if save tripDTO
-     *
-     */
-    @PostMapping(path = "/create", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
-    public ResponseEntity<Void> create(@Valid @RequestBody TripDTO tripDTO) throws MobithinkBusinessException {
-
-        Trip savedTrip = saveNewTrip(tripDTO);
-        List<StationData> stationDataList = saveStationDataList(savedTrip, tripDTO.getStationDataDTOList());
-        saveEventList(savedTrip, tripDTO.getEventDTOList(), stationDataList);
-        saveRollingPointList(savedTrip, tripDTO.getRollingPointDTOList());
-
-        return new ResponseEntity<>(null, new HttpHeaders(), HttpStatus.CREATED);
-    }
-
-    /**
-     *
-     * GET. find TripDTO associated at city and line.
-     *
-     * @param cityName and lineName
-     *
-     * @return List<TripDTO> or null if not exist Trip saved for this busLine
-     *
-     */
-    @GetMapping(path = "/find/{busLineId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
-    public ResponseEntity<List<TripDTO>> create(@Valid @PathVariable Long busLineId) throws MobithinkBusinessException {
-
-        List<Trip> tripList = tripService.findTripListByBusLineId(busLineId);
-        List<TripDTO> tripDTOList = new ArrayList<>();
-
-        if (tripList != null){
-            for (Trip trip : tripList){
-                TripDTO tripDTO = createTripDTO(trip);
-                tripDTOList.add(tripDTO);
-            }
-            return ResponseEntity.ok(tripDTOList);
-        } else return null;
-    }
+	@Resource
+	BusLineService busLineService;
 
 
-    private TripDTO createTripDTO(Trip trip) {
+	/**
+	 *
+	 * POST. Register a new TripDTO.
+	 *
+	 * @param tripDTO object
+	 *
+	 * @return status 201 if save tripDTO
+	 *
+	 */
+	@PostMapping(path = "/create", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<Void> create(@Valid @RequestBody TripDTO tripDTO) throws MobithinkBusinessException {
 
-        List<StationDataDTO> stationDataDTOList = stationDataService.findAllStationDataDtoByTripId(trip.getId());
-        List<EventDTO> eventDTOList = eventService.findAllEventDTObyTripId(trip.getId());
-        List<RollingPointDTO> rollingPointDTOList = rollingPointService.findRollingPOintDtoListByTripId(trip.getId());
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(tripDTO.getStartTime());
+		
+		BusLine busLine = busLineService.findOneById(tripDTO.getBusLineid());
+		StringBuilder sb = new StringBuilder();
+		sb.append(busLine.getCity().getName())
+		.append("/")
+		.append(busLine.getName())
+		.append(" (")
+		.append(calendar.get(Calendar.DAY_OF_MONTH))
+		.append("/")
+		.append(calendar.get(Calendar.MONTH)+1)
+		.append("/")
+		.append(calendar.get(Calendar.YEAR))
+		.append(")");
+		
+		tripDTO.setTripName(sb.toString());
+		Trip savedTrip = saveNewTrip(tripDTO);
+		List<StationData> stationDataList = saveStationDataList(savedTrip, tripDTO.getStationDataDTOList());
+		saveEventList(savedTrip, tripDTO.getEventDTOList(), stationDataList);
+		saveRollingPointList(savedTrip, tripDTO.getRollingPointDTOList());
 
-        return ConverterOfDTO.convertTripToTripDTO(trip, stationDataDTOList, eventDTOList, rollingPointDTOList);
-    }
+		return new ResponseEntity<>(null, new HttpHeaders(), HttpStatus.CREATED);
+	}
+
+	/**
+	 *
+	 * GET. find TripDTO associated at city and line.
+	 *
+	 * @param cityName and lineName
+	 *
+	 * @return List<TripDTO> or null if not exist Trip saved for this busLine
+	 *
+	 */
+	@GetMapping(path = "/find/{busLineId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<List<TripDTO>> create(@Valid @PathVariable Long busLineId) throws MobithinkBusinessException {
+
+		List<Trip> tripList = tripService.findTripListByBusLineId(busLineId);
+		List<TripDTO> tripDTOList = new ArrayList<>();
+
+		if (tripList != null){
+			for (Trip trip : tripList){
+				TripDTO tripDTO = createTripDTO(trip);
+				tripDTOList.add(tripDTO);
+			}
+			return ResponseEntity.ok(tripDTOList);
+		} else return null;
+	}
 
 
-    private void saveRollingPointList(Trip savedTrip, List<RollingPointDTO> rollingPointDTOList) {
+	private TripDTO createTripDTO(Trip trip) {
 
-        for (RollingPointDTO rollingPointDto : rollingPointDTOList){
-            RollingPoint rollingPoint = new RollingPoint();
-            rollingPoint.setTrip(savedTrip);
-            rollingPoint.setGpsLat(rollingPointDto.getGpsLat());
-            rollingPoint.setGpsLong(rollingPointDto.getGpsLong());
-            rollingPoint.setTimeOfRollingPoint(rollingPointDto.getPointTime());
-            rollingPoint.setTraffic(rollingPointDto.getTrafficIndex());
+		List<StationDataDTO> stationDataDTOList = stationDataService.findAllStationDataDtoByTripId(trip.getId());
+		List<EventDTO> eventDTOList = eventService.findAllEventDTObyTripId(trip.getId());
+		List<RollingPointDTO> rollingPointDTOList = rollingPointService.findRollingPOintDtoListByTripId(trip.getId());
 
-            rollingPointService.createRollingPoint(rollingPoint);
-        }
-    }
-
-    private void saveEventList(Trip savedTrip, List<EventDTO> eventDTOList, List<StationData> stationDataList) {
-        Event event = new Event();
-        event.setTrip(savedTrip);
-        for (EventDTO eventDto : eventDTOList) {
-            event.setEndTime(eventDto.getEndTime());
-            event.setStartTime(eventDto.getStartTime());
-            event.setEventName(eventDto.getEventName());
-            if (eventDto.getStationName() != null) {
-                for (StationData stationData : stationDataList){
-                    if(stationData.getStationName().equals(eventDto.getStationName())){
-                        event.setStationData(stationData);
-                    }
-                }
-            }
-            event.setGpsLat(eventDto.getGpsLat());
-            event.setGpsLong(eventDto.getGpsLong());
-
-            Event savedEvent = eventService.createEvent(event);
-            savePictureList(eventDto.getPictureIdList(),savedEvent.getId(),null);
+		return ConverterOfDTO.convertTripToTripDTO(trip, stationDataDTOList, eventDTOList, rollingPointDTOList);
+	}
 
 
-        }
-    }
+	private void saveRollingPointList(Trip savedTrip, List<RollingPointDTO> rollingPointDTOList) {
 
-    private void savePictureList(List<Long> pictureIdList, Long savedEventId, Long savedStationDataId) {
-        if (pictureIdList != null){
-            for (Long pictureID : pictureIdList){
-                Picture picture = new Picture();
-                if (savedEventId != null){
-                    picture.setEventId(savedEventId);
-                }
-                if (savedStationDataId != null){
-                    picture.setStationDataId(savedStationDataId);
-                }
-                picture.setPictureId(pictureID);
-                pictureService.savedPicture(picture);
-            }
-        }
-    }
+		if(savedTrip!=null && rollingPointDTOList!=null){
+			for (RollingPointDTO rollingPointDto : rollingPointDTOList){
+				RollingPoint rollingPoint = new RollingPoint();
+				rollingPoint.setTrip(savedTrip);
+				rollingPoint.setGpsLat(rollingPointDto.getGpsLat());
+				rollingPoint.setGpsLong(rollingPointDto.getGpsLong());
+				rollingPoint.setTimeOfRollingPoint(rollingPointDto.getPointTime());
+				rollingPoint.setTraffic(rollingPointDto.getTrafficIndex());
 
-    private List<StationData> saveStationDataList(Trip savedTrip, List<StationDataDTO> stationDataDTOList) {
-        int counter = 0;
+				rollingPointService.createRollingPoint(rollingPoint);
+			}
+		}
+	}
 
-        List<StationData> stationDataList = new ArrayList<>();
+	private void saveEventList(Trip savedTrip, List<EventDTO> eventDTOList, List<StationData> stationDataList) {
 
-        for (StationDataDTO stationDataDTO : stationDataDTOList ){
+		if(savedTrip!=null && eventDTOList!=null && stationDataList!=null){
+			Event event = new Event();
+			event.setTrip(savedTrip);
+			for (EventDTO eventDto : eventDTOList) {
+				event.setEndTime(eventDto.getEndTime());
+				event.setStartTime(eventDto.getStartTime());
+				event.setEventName(eventDto.getEventName());
+				if (eventDto.getStationName() != null) {
+					for (StationData stationData : stationDataList){
+						if(stationData.getStationName().equals(eventDto.getStationName())){
+							event.setStationData(stationData);
+						}
+					}
+				}
+				event.setGpsLat(eventDto.getGpsLat());
+				event.setGpsLong(eventDto.getGpsLong());
 
-            StationData stationData = new StationData();
-            stationData.setStationName(stationDataDTO.getStationName());
-            stationData.setEndTime(stationDataDTO.getEndTime());
-            stationData.setStartTime(stationDataDTO.getStartTime());
-            stationData.setNumberOfComeIn(stationDataDTO.getNumberOfComeIn());
-            stationData.setNumberOfGoOut(stationDataDTO.getNumberOfGoOut());
-            stationData.setGpsLat(stationDataDTO.getGpsLat());
-            stationData.setGpsLong(stationDataDTO.getGpsLong());
-            stationData.setStationStep(counter);
-            stationData.setTrip(savedTrip);
+				Event savedEvent = eventService.createEvent(event);
+				savePictureList(eventDto.getPictureIdList(),savedEvent.getId(),null);
+			}
+		} 
 
-            StationData savedStationData = stationDataService.createStationData(stationData);
-            stationDataList.add(savedStationData);
 
-            savePictureList(stationDataDTO.getPictureIdList(), null, savedStationData.getId());
+	}
 
-            counter++;
-        }
-        return stationDataList;
-    }
+	private void savePictureList(List<Long> pictureIdList, Long savedEventId, Long savedStationDataId) {
+		if (pictureIdList != null){
+			for (Long pictureID : pictureIdList){
+				Picture picture = new Picture();
+				if (savedEventId != null){
+					picture.setEventId(savedEventId);
+				}
+				if (savedStationDataId != null){
+					picture.setStationDataId(savedStationDataId);
+				}
+				picture.setPictureId(pictureID);
+				pictureService.savedPicture(picture);
+			}
+		}
+	}
 
-    private Trip saveNewTrip(TripDTO tripDTO) {
+	private List<StationData> saveStationDataList(Trip savedTrip, List<StationDataDTO> stationDataDTOList) {
 
-        Trip savedTrip = new Trip();
-        savedTrip.setWeather(tripDTO.getWeather());
-        savedTrip.setAtmo(tripDTO.getAtmo());
-        savedTrip.setBusLine(busLineService.findOneById(tripDTO.getBusLineid()));
-        savedTrip.setStartGpsLat(tripDTO.getStartGpsLat());
-        savedTrip.setStartGpsLong(tripDTO.getStartGpsLong());
-        savedTrip.setEndGpsLat(tripDTO.getEndGpsLat());
-        savedTrip.setEndGpsLong(tripDTO.getEndGpsLong());
-        savedTrip.setStartTime(tripDTO.getStartTime());
-        savedTrip.setEndTime(tripDTO.getEndTime());
-        savedTrip.setTemperature(tripDTO.getTemperature());
-        savedTrip.setTripName(tripDTO.getTripName());
-        savedTrip.setVehicleCapacity(tripDTO.getVehicleCapacity());
+		if(savedTrip != null && stationDataDTOList != null){
+			int counter = 0;
 
-        return tripService.createTrip(savedTrip);
-    }
+			List<StationData> stationDataList = new ArrayList<>();
+
+			for (StationDataDTO stationDataDTO : stationDataDTOList ){
+
+				StationData stationData = new StationData();
+				stationData.setStationName(stationDataDTO.getStationName());
+				stationData.setEndTime(stationDataDTO.getEndTime());
+				stationData.setStartTime(stationDataDTO.getStartTime());
+				stationData.setNumberOfComeIn(stationDataDTO.getNumberOfComeIn());
+				stationData.setNumberOfGoOut(stationDataDTO.getNumberOfGoOut());
+				stationData.setGpsLat(stationDataDTO.getGpsLat());
+				stationData.setGpsLong(stationDataDTO.getGpsLong());
+				stationData.setStationStep(counter);
+				stationData.setTrip(savedTrip);
+
+				StationData savedStationData = stationDataService.createStationData(stationData);
+				stationDataList.add(savedStationData);
+
+				savePictureList(stationDataDTO.getPictureIdList(), null, savedStationData.getId());
+
+				counter++;
+			}
+			return stationDataList;
+		} else return null;
+	}
+
+	private Trip saveNewTrip(TripDTO tripDTO) {
+
+		Trip savedTrip = new Trip();
+		savedTrip.setWeather(tripDTO.getWeather());
+		savedTrip.setAtmo(tripDTO.getAtmo());
+		savedTrip.setBusLine(busLineService.findOneById(tripDTO.getBusLineid()));
+		savedTrip.setStartGpsLat(tripDTO.getStartGpsLat());
+		savedTrip.setStartGpsLong(tripDTO.getStartGpsLong());
+		savedTrip.setEndGpsLat(tripDTO.getEndGpsLat());
+		savedTrip.setEndGpsLong(tripDTO.getEndGpsLong());
+		savedTrip.setStartTime(tripDTO.getStartTime());
+		savedTrip.setEndTime(tripDTO.getEndTime());
+		savedTrip.setTemperature(tripDTO.getTemperature());
+		savedTrip.setTripName(tripDTO.getTripName());
+		savedTrip.setVehicleCapacity(tripDTO.getVehicleCapacity());
+
+		return tripService.createTrip(savedTrip);
+	}
 }
